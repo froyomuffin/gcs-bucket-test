f577c4f94a57ed5bc1dc34201fe81504c9d93fba
Thomas Wang <tw@eclair.io>
2021-06-19 18:34:52 +0800

    Add sample reconciliation

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'bundler/setup'
 require "google/cloud/storage"
 require "stringio"
+require "json"
 
 class Client
   def initialize
@@ -13,32 +14,80 @@ class Client
     @bucket = storage.bucket "froyomuffin-test-bucket"
   end
 
-  def write
-    puts "Write"
-    content = {
-      name: "Potato",
-      address: "15 Potato",
-    }
+  def write(content, filename)
+    puts "Writing #{filename}"
 
-    filename = 'test_file.json'
-
-    file = @bucket.create_file(
+    @bucket.create_file(
       StringIO.new(content.to_json),
       filename
     )
   end
 
-  def read
-    puts "Read"
-    filename = 'test_file.json'
+  def read(filename)
+    puts "Reading #{filename}"
 
     file = @bucket.file(filename)
+    content = file.download
 
-    puts file.download.string
+    puts content.string
+    puts file.signed_url
+
+    content
+  end
+
+  def create_test_file
+    prefix = timestamp
+    content = 10000.times.map do |iteration|
+      { value: "#{prefix}-#{iteration}" }
+    end
+
+    filename = "#{temp_prefix}/#{timestamp}.json"
+
+    write(content, filename)
+  end
+
+  def create_test_final_file
+    result = @bucket.files(prefix: temp_prefix).flat_map do |file|
+      content = file.download
+      JSON.parse(content.string)
+    end
+
+    filename = "#{final_prefix}/#{timestamp}.json"
+
+    puts result.to_json
+
+    write(result.to_json, filename)
+  end
+
+  def reset_bucket
+    puts "Reset"
+
+    @bucket.files.each(&:delete)
+  end
+
+  def timestamp
+    Time.now.to_i
+  end
+
+  def temp_prefix
+    "temp"
+  end
+
+  def final_prefix
+    "final"
   end
 end
 
 puts "Starting"
 client = Client.new
-#client.write
-client.read
+
+# Create 10 files each with 10000 records
+10.times do
+  client.create_test_file
+end
+
+# Stitch them
+result = client.create_test_final_file
+
+# Get a signed URL
+puts result.signed_url
